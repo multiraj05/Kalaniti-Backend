fe884677ce5f5ab41568b64836c8221a1470c4d6
multiraj05 <multikodraj05@gmail.com>
2024-04-15 13:57:12 +0530

    ok

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -197,6 +197,7 @@ exports.createOrder = async (req, res) => {
 //         return response(res, 500, { status: false, message: "Internal Server Error" });
 //     }
 // };
+
 exports.updateOrderStatus = async (req, res) => {
   try {
     const { id } = req.query
@@ -237,43 +238,95 @@ exports.getOrders = async (req, res) => {
         const skip = page * limit;
         const { userId, orderId, search } = req.query;
 
-        if (orderId) {
-            console.log("Fetching order by ID:", orderId);
+        let matchQuery = {};
 
-            const order = await Order.findById(orderId);
-            if (!order) {
-                console.log("Order not found.");
-                return response(res, 401, { status: false, message: 'Order not found' });
-            }
-            console.log("Order found:", order);
-
-            const cart = await cartDetails.findById(order.cartId);
-            if (!cart) {
-                console.log("Cart not found for the order.");
-                return response(res, 404, { status: false, message: 'Cart not found for the order' });
-            }
-            console.log("Cart found:", cart);
-
-            const product = await productInfo.findById(cart.productId);
-            if (!product) {
-                console.log("Product not found for the cart item.");
-                return response(res, 404, { status: false, message: 'Product not found for the cart item' });
-            }
-            console.log("Product found:", product);
-
-            const orderDetails = {
-                productId: product._id,
-                name: product.productName,
-                price: cart.quantity * product.price1,
-                status: order.status,
-                quantity: cart.quantity,
-                productDetails: product 
+        if (userId) {
+            matchQuery = {
+                userId: new mongoose.Types.ObjectId(userId)
             };
-            order.products = [orderDetails];
-            console.log("Order details:", order);
-
-            return response(res, 200, { status: true, message: 'Order retrieved successfully', order: orderDetails });
         }
+
+        if (!orderId && !userId) {
+            return response(res, 400, { status: false, message: "Please provide orderId or userId" });
+        }
+
+        const commonPipeline = [
+            {
+                $match: matchQuery,
+            },
+            {
+                $lookup: {
+                    from: "cartdetails",
+                    localField: "cartId",
+                    foreignField: "_id",
+                    as: "cartDetails",
+                },
+            },
+            {
+                $unwind: "$cartDetails",
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    localField: "cartDetails.productId",
+                    foreignField: "_id",
+                    as: "productDetails",
+                },
+            },
+            {
+                $unwind: "$productDetails",
+            },
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "productDetails.categoryId",
+                    foreignField: "_id",
+                    as: "categoryDetails",
+                },
+            },
+            {
+                $unwind: "$categoryDetails",
+            },
+            {
+                $project: {
+                    "_id":1,
+                    "userId":1,
+                    "cartId":1,
+                    "status":1,
+                    "totalPrice":1,
+                    "quantity":"$cartDetails.quantity",
+                    "size":"$cartDetails.size",
+                    "productCode": "$productDetails.productCode",
+                    "productName": "$productDetails.productName",
+                    "title":"$productDetails.title",
+                    "images":"$productDetails.images",
+                    "price1":"$productDetails.price1",
+                    "price2":"$productDetails.price2",
+                    "category":"$categoryDetails.name",
+                    "createdAt":1,
+                }
+            }
+        ];
+
+        const countPipeline = [...commonPipeline, { $count: "totalCount" }];
+        const aggregationPipeline = [
+            ...commonPipeline,
+            { $skip: skip },
+            { $limit: limit },
+            { $sort: { createdAt: -1 } },
+        ];
+
+        const countResult = await Order.aggregate(countPipeline);
+        const totalCount = countResult[0]?.totalCount || 0;
+
+        const Result = await Order.aggregate(aggregationPipeline);
+        const paginatedResults = Result || [];
+
+        return response(res, 200, {
+            message: "Orders retrieved successfully!",
+            orders: paginatedResults,
+            ordersTotal: totalCount,
+        });
     } catch (error) {
         console.error("Error fetching orders:", error);
         return response(res, 500, { status: false, message: "Internal Server Error" });
